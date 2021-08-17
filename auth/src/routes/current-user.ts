df7c011dd8930e8ab91c10b5273932f6071a2943
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-08-17 22:00:39 +0500

    Added Current User middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,12 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 
+import { currentUser } from '../middlewares/current-user'
+
 const router = express.Router()
 
-router.get(`/api/users/currentuser`, async (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null })
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-    return res.send({ currentUser: payload })
-  } catch (err) {
-    return res.send({ currentUser: null })
-  }
+router.get(`/api/users/currentuser`, currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null })
 })
 
 export { router as currentUserRouter }
