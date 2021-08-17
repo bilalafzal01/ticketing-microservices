bc4b77cfa7657abf0644c27aa878abfb25c52e10
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-08-17 17:00:42 +0500

    JWT Secret Key setup + signup update

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
+
 import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { BadRequestError } from '../errors/bad-request-error'
@@ -33,6 +35,13 @@ router.post(
 
     const user = User.build({ email, password })
     await user.save()
+
+    // * Generate JWT and store it on the session object
+    const userJwt = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    )
+    req.session = { jwt: userJwt }
 
     res.status(201).send(user)
   }
