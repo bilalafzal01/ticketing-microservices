5a887ff99cd46b925dd273f3d73b9d6b20a75cba
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-08-17 17:39:14 +0500

    Signin feature added

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import jwt from 'jsonwebtoken'
+
+import { User } from '../models/user'
 import { validateRequest } from '../middlewares/validate-request'
+import { BadRequestError } from '../errors/bad-request-error'
+import { Password } from '../services/password'
 
 const router = express.Router()
 
@@ -14,7 +19,32 @@ router.post(
       .withMessage('Password must be supplied!'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body
+    const existingUser = await User.findOne({ email })
+    if (!existingUser) {
+      throw new BadRequestError(`Invalid credentials`)
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    )
+    if (!passwordsMatch) {
+      throw new BadRequestError(`Invalid credentials`)
+    }
+
+    // * Generate JWT and store it in the cookie
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY!
+    )
+    req.session = {
+      jwt: userJwt,
+    }
+
+    res.status(200).send(existingUser)
+  }
 )
 
 export { router as signinRouter }
