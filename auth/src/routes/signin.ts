5bb2c88b2cb593c46ef9976d2451fd2c5c578f40
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-08-17 17:28:59 +0500

    Validate Request middleware added

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,20 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
-router.post(`/api/users/signin`, (req, res) => {
-  res.send(`Hey there :)`)
-})
+router.post(
+  `/api/users/signin`,
+  [
+    body('email').isEmail().withMessage('Email must be valid!'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('Password must be supplied!'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {}
+)
 
 export { router as signinRouter }
