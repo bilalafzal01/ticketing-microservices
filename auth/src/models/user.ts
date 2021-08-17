c6fe4e4eb89d87c36e77549bf63c75d1673bb4f2
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-08-17 17:15:28 +0500

    Formatted JSON return properties

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -18,16 +18,28 @@ interface UserDoc extends mongoose.Document {
   password: string
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-})
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id
+        delete ret._id
+        delete ret.password
+        delete ret.__v
+      },
+    },
+  }
+)
 
 // * using function keyword instead of arrow function because in case of arrow functions,
 // * the 'this' keyword refers to the context of whole file. Not what we want.
