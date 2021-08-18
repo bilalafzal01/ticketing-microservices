72d0fc213beb473efa16b0893dd32ff6a93f12c7
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-08-18 14:55:37 +0500

    Feature: Next.js client setup

---
+++ client/pages/banana.js
@@ -0,0 +1,5 @@
+const Banana = () => {
+  return <h1>hehehehehdsaf hooohoo dsf</h1>
+}
+
+export default Banana
---
+++ client/pages/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Hey!</h1>
+}
+
+export default Home
