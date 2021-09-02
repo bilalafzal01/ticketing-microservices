fd56ee53ecfa7079528d1ac86271c956e908b076
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-09-02 23:04:29 +0500

    fix(deps): updated dependencies

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -3,8 +3,6 @@ import mongoose from 'mongoose'
 import jwt from 'jsonwebtoken'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 
-import { app } from '../app'
-
 // * to tell typescript that there is a global function named `signin`
 declare global {
   var signin: () => string[]
