64a9f7454a4fac6e62bd943652a58222f63546ad
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-09-03 06:38:42 +0500

    feat(orders): scaffold orders service starter

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+import { currentUser, errorHandler, NotFoundError } from '@mafzaltickets/common'
+
+import { newOrderRouter } from './routes/new'
+import { showOrderRouter } from './routes/show'
+import { indexOrderRouter } from './routes/index'
+import { deleteOrderRouter } from './routes/delete'
+
+const app = express()
+app.set(`trust proxy`, true)
+
+app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+)
+// * sets the req.currentUser if authenticated
+app.use(currentUser)
+
+app.use(newOrderRouter)
+app.use(showOrderRouter)
+app.use(indexOrderRouter)
+app.use(deleteOrderRouter)
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
