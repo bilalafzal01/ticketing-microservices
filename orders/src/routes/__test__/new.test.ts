3bf8fdee0e20ce6dca991dd6c376120ec8b06e28
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-09-03 22:14:27 +0500

    feat(order): order post tests completed

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose'
+import request from 'supertest'
+import { app } from '../../app'
+import { natsWrapper } from '../../nats-wrapper'
+import { Order } from '../../models/order'
+import { Ticket } from '../../models/ticket'
+import { OrderStatus } from '@mafzaltickets/common'
+
+it('has a route handler listening to /api/orders for post requests', async () => {
+  const response = await request(app).post(`/api/orders`).send({})
+  expect(response.status).not.toEqual(404)
+})
+
+it('can only be accessed if the user is signed in', async () => {
+  await request(app).post(`/api/orders`).send({}).expect(401)
+})
+
+it('returns a status other than 401 if the user is signed in', async () => {
+  const response = await request(app)
+    .post(`/api/tickets`)
+    .set('Cookie', global.signin())
+    .send({})
+
+  expect(response.status).not.toEqual(401)
+})
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = mongoose.Types.ObjectId()
+
+  await request(app)
+    .post(`/api/orders`)
+    .set('Cookie', global.signin())
+    .send({ ticketId })
+    .expect(404)
+})
+
+it('returns an error if the ticket is already reserved', async () => {
+  const ticket = Ticket.build({ title: 'concert', price: 20 })
+  await ticket.save()
+
+  const order = Order.build({
+    ticket,
+    userId: 'asdf',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  })
+  await order.save()
+
+  await request(app)
+    .post(`/api/orders`)
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400)
+})
+
+it('reserves a ticket', async () => {
+  const ticket = Ticket.build({ title: 'concert', price: 20 })
+  await ticket.save()
+
+  await request(app)
+    .post(`/api/orders`)
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201)
+})
---
+++ orders/src/test/setup.ts
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+
+// * to tell typescript that there is a global function named `signin`
+declare global {
+  var signin: () => string[]
+}
+
+let mongo: any
+
+// * for mock purposes
+jest.mock(`../nats-wrapper.ts`)
+
+// * hook function that runs before the tests start
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf'
+
+  mongo = await MongoMemoryServer.create()
+  const mongoUri = await mongo.getUri()
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+})
+
+// * hook that runs before each of our test
+beforeEach(async () => {
+  // * clear all mocks
+  jest.clearAllMocks()
+
+  const collections = await mongoose.connection.db.collections()
+  for (let collection of collections) {
+    await collection.deleteMany({})
+  }
+})
+
+// * hook that runs after all of our tests have finished
+afterAll(async () => {
+  await mongo.stop()
+  await mongoose.connection.close()
+})
+
+// * global function; only available in the test environment because
+// * we are making it in this setup.ts file
+global.signin = () => {
+  // * Build a JWT payload. {id, email}
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+  }
+  // * Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!)
+
+  // * Build a session object {jwt: MY_JWT}
+  const session = { jwt: token }
+
+  // * Turn that session into JSON
+  const sessionJSON = JSON.stringify(session)
+
+  // * Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64')
+
+  // * return a string that is the cookie with the encoded data
+  return [`express:sess=${base64}`]
+}
